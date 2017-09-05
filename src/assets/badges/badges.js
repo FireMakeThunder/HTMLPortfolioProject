e14c79132099a956448be7e7ff1e297f3b16441b
Elias <elias.fraley@gmail.com>
2017-09-04 19:29:07 -0700

    Added badges to portfolio
    Directory structure:
    Moved css and js to assets.
    Folders are modules
    
    Header:
    add Bootstrap css
    add Badges css
    
    Body:
    add Badges div
    add Badges js

---
+++ src/assets/badges/badges.js
@@ -0,0 +1,40 @@
+$(function() {
+
+  $.ajax({
+    url: 'https://www.codeschool.com/users/FireMakeThunder.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      console.log('response', response);
+      console.log(response.courses.completed);
+      addCourses(response.courses.completed);
+    }
+  })
+
+  function addCourses(courses) {
+
+    var $badges = $('#badges');
+
+    courses.forEach(function(course) {
+
+      var $course = $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($course);
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo($course);
+
+      $('<a />', {
+        'class': 'btn btn-primary',
+        target: '_blank',
+        href: course.url,
+        text: 'See Course'
+      }).appendTo($course);
+
+    });
+  }
+});
